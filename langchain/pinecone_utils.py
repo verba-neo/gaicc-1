9a624b993e77a360f040306a79cb970da080317f
verba-neo <verba.it.corp@gmail.com>
2024-07-17 11:54:35 +0900

    pinecone vs

--- langchain/pinecone_utils.py
+++ langchain/pinecone_utils.py
@@ -1,10 +1,24 @@
 # pinecone_utils.py
-
 import os
 from dotenv import load_dotenv
-
+from langchain_pinecone import PineconeVectorStore
+from langchain_openai import OpenAIEmbeddings
 load_dotenv()
 
 PINECONE_API_KEY = os.getenv('PINECONE_API_KEY')
 
-print(PINECONE_API_KEY)
+
+def create_embeddings(index_name, docs):
+    PineconeVectorStore.from_documents(
+        documents=docs,
+        embedding=OpenAIEmbeddings(),
+        index_name=index_name
+    )
+
+
+def get_vectorstore(index_name):
+    vs = PineconeVectorStore.from_existing_index(
+        index_name=index_name,
+        embedding=OpenAIEmbeddings()
+    )
+    return vs
